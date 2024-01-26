84dbd03489453ee491c33348d77468a00fb3ac2f
Zachary Huang <142925256+zachdh@users.noreply.github.com>
2024-01-25 18:31:39 -0800

    PlayingCards WIP

--- components/PlayingCards.js
+++ components/PlayingCards.js
@@ -1,3 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import {Text, Pressable, StyleSheet} from 'react-native'
 
+export default function PlayingCards() {
+    const fetchData = async() => {
+        
+        const response = await fetch('https://opentdb.com/api.php?amount=1&category=18&difficulty=hard&type=multiple')
+        const data = await response.json()
+    }
+    return(
+        <Text>hello</Text>
+    );
+}
