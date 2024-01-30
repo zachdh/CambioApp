43316c49f5cf768dac1adf96a6605422a7a9acac
Zachary Huang <142925256+zachdh@users.noreply.github.com>
2024-01-30 09:14:35 -0800

    Landing Page Complete

--- components/HowToPlayButton.js
+++ components/HowToPlayButton.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Text, Pressable, StyleSheet} from 'react-native'
 
-import {useFonts} from 'expo-font'
+import * as Font from 'expo-font'
 
 export default function HowToPlayButton(props){ 
-
-  const [Font] = useFonts({
-    'Miltonian Tattoo': require('./../assets/fonts/MiltonianTattoo-Regular.ttf')
-  });
+  //async loading font
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          'Miltonian Tattoo': require('./../assets/fonts/MiltonianTattoo-Regular.ttf'),
+        });
+        setFontsLoaded(true);
+      } catch (error) {
+        console.error('Error loading fonts:', error);
+      }
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    // You can render a loading indicator or return null while the fonts are loading
+    return null;
+  }
+
+  //customize pressable
   const { onPress, title= 'How to Play' } = props;
     return(
       <Pressable style={[styles.button, styles.shadowProp]} onPress={(onPress)}>
--- components/PlayButton.js
+++ components/PlayButton.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Text, Pressable, StyleSheet} from 'react-native'
 
-import {useFonts} from 'expo-font'
+import * as Font from 'expo-font'
 
 export default function PlayButton(props){ 
-  const [fonts] = useFonts({
-      'Miltonian Tattoo': require('./../assets/fonts/MiltonianTattoo-Regular.ttf'),
-  });
+  //async loading font
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          'Miltonian Tattoo': require('./../assets/fonts/MiltonianTattoo-Regular.ttf'),
+        });
+        setFontsLoaded(true);
+      } catch (error) {
+        console.error('Error loading fonts:', error);
+      }
+    };
 
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    // You can render a loading indicator or return null while the fonts are loading
+    return null;
+  }
+
+  //customize pressable
   const { onPress, title= 'Play' } = props;
   return(
     <Pressable style={[styles.button, styles.shadowProp]} onPress={(onPress)}>
